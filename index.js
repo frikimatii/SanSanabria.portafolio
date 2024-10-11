58a055b1dd81a6724fc293e2eabe3fcc9dce10c6
Frikimatii7 <matibiencomodo@gmail.com>
2024-10-11 07:08:44 -0300

    primercomint001

---
+++ index.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal(".imagenuno", {delay: 375,duration: 500,reset: true,});
+
+ScrollReveal().reveal(".sobremi", { easing: "ease-in", reset: true,duration: 500, });
+
+ScrollReveal().reveal(".quees", { distance: '50px', opacity: 0.8,reset: true });
+
+ScrollReveal().reveal('.portafolio', { scale: 0.85,reset: true  });
+
+ScrollReveal().reveal('.img1', { scale: 0.85,reset: true ,delay: 500 });
+
+ScrollReveal().reveal('.oferta', { origin: 'right', scale: 0.90,reset: true });
+
+
+
+ScrollReveal().reveal(".nofert1", {delay: 200,duration: 300,reset: true,});
+
+ScrollReveal().reveal(".nofert2", {delay: 400,duration: 500,reset: true,});
+
+ScrollReveal().reveal(".nofert3", {delay: 600,duration: 700,reset: true,});
+
+ScrollReveal().reveal(".nofert4", {delay: 800,duration: 900,reset: true,});
+
+ScrollReveal().reveal(".nofert5", {delay: 1000,duration: 1100,reset: true,});
+
+
+ScrollReveal().reveal('.skill', { origin: 'top', scale: 0.90, reset: true });
+
+ScrollReveal().reveal('.nicho', {  easing: "ease-in", reset: true,delay: 400, });
+ScrollReveal().reveal('.habilidades', {  easing: "ease-in", reset: true,delay: 400, });
